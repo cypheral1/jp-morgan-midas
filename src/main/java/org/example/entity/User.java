0dbac84a0d5053844848e7d009dc05ef4bebd317
cypheral1 <studiessameer3@gamil.com>
2025-10-29 13:58:22 +0530

    xx

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,50 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private double balance;
+
+    public User() {
+    }
+
+    public User(String username, double balance) {
+        this.username = username;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
